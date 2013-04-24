bb2b8953fca128b00f2d7372fa16a729aa76e15e
Podshot <ben.gothard3@gmail.com>
2013-04-24 12:39:20 -0300

    Update Command Block Namer.py
    Added Comments

--- Command Block Namer.py
+++ Command Block Namer.py
@@ -1,3 +1,8 @@
+# This filter renames command blocks
+# This was created by Podshot
+# If you modify this filter, please give credit to Podshot
+# Have an idea? Can you improve this code? Fork the Github!
+# Link: https://github.com/Podshot/MCEdit-Filters
 from pymclevel import MCSchematic
 from pymclevel import TileEntity
 from pymclevel import TAG_Compound
@@ -12,11 +17,12 @@ displayName = "Renames Command Blocks"
 inputs = (
         ("Custom Name:", "string"),
 )
-
+# Asks for user input
 def perform(level, box, options):
     name = options["Custom Name:"]
-
+    
     for (chunk, slices, point) in level.getChunkSlices(box):
+        # Looks for Tile Entities in the Selection Box
         for t in chunk.TileEntities:
             x = t["x"].value
             y = t["y"].value
@@ -26,6 +32,8 @@ def perform(level, box, options):
                 if "Command" in t:
                                         if "CustomName" not in t:
                                             t["CustomName"] = TAG_String()
+                                        # Checks to see if specified tag exists
 
                                         t["CustomName"] = TAG_Byte(name)
                                         chunk.dirty = True
+                                        # Ends filter and marks chunk as changed
