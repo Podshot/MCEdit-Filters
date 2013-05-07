501c7e4f0c38e99aa3b7eaaca1cbe94529462b49
Podshot <ben.gothard3@gamil.com>
2013-05-07 13:11:59 -0400

    Added Demo Folder and Demo Filter
    Added Demo folder and added Set Block.py
    Which is a very bad filter version of the "fill" tool
    Comments will be added soon

---
+++ Demo/Set Block.py
@@ -0,0 +1,21 @@
+from pymclevel import MCSchematic
+from pymclevel import alphaMaterials
+import time
+
+displayName = "Demo Filter: Block Gen"
+
+inputs = (
+        ("Material:", alphaMaterials),
+        ("A filter version of the Fill Tool", "label"),
+)
+def setBlock(level, (block, data), x, y, z):
+    level.setBlockAt(x, y, z, block)
+    level.setBlockDataAt(x, y, z, data)
+def perform(level, box, options):
+    method = "Fill-ter"
+    print '%s: Started: %s' % (method, time.ctime())
+    mat = (options["Material:"].ID, options["Material:"].blockData)
+    setBlock(level, mat, box.minx, box.miny, box.minz)
+    setBlock(level, mat, box.maxx, box.maxy, box.maxz)
+    level.markDirtyBox(box)
+
