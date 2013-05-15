69efa1def0765171af0a5e1e6172b442da876658
Podshot <ben.gothard3@gmail.com>
2013-05-15 16:42:04 -0500

    Created Minecaft Creator.py
    Note: Not Finished, hence why its in the Work in Progress Folder

---
+++ Work in Progress/Minecart Creator.py
@@ -0,0 +1,15 @@
+from pymclevel import TAG_Compound
+from pymclevel import TAG_Int
+from pymclevel import TAG_Short
+from pymclevel import TAG_Byte
+from pymclevel import TAG_String
+from pymclevel import TAG_Float
+from pymclevel import TAG_Double
+from pymclevel import TAG_List
+
+displayName = "Custom Minecart Creator"
+
+inputs = (
+    ("Custom Block:", alphaMaterials),
+    ("Block Height from Cart",
+    
