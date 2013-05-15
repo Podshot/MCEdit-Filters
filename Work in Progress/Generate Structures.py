7d2eb75ae8b3c4830ca750fb43833f9c800c9c21
Podshot <ben.gothard3@gamil.com>
2013-05-15 13:01:55 -0400

    Continue refactoring of Generate Structures.py
    Refactored block generation into a function and change variable names.

--- Work in Progress/Generate Structures.py
+++ Work in Progress/Generate Structures.py
@@ -11,18 +11,19 @@ from pymclevel import TAG_String
 from math import sqrt
 from numpy import zeros
 import random
+import time
 
-displayName = "Generate Structure"
+displayName = "Generate Structures"
 
 Build = {
-    "Blacksmith": "smith",
-    "Church": "church",
-    "Library": "lib",
-    "House #1": "h1",
-    "House #2": "h2",
-    "House #3": "h3",
-    "Fountain": "foun",
-    "Farm": "farm",
+    "Blacksmith": 1,
+    "Church": 2,
+    "Library": 3,
+    "House #1": 4,
+    "House #2": 5,
+    "House #3": 6,
+    "Fountain": 7,
+    "Farm": 8,
     }
 
 inputs = (
@@ -30,152 +31,162 @@ inputs = (
         ("Note: This filter does not generate doors", "label"),
 )
 
-def blacksmith(level, box, options):
+def house1(level, x, y, z):
+    level.setBlockAt(x, y, z, 4)
+    level.setBlockDataAt(x, y, z, 0)
+    level.setBlockAt(x-1, y, z, 67)
+    level.setBlockDataAt(x-1, y, z, 0x0)
+    level.setBlockAt(x+1, y, z, 4)
+    level.setBlockDataAt(x+1, y, z, 0)
+    level.setBlockAt(x+2, y, z, 4)
+    level.setBlockDataAt(x+2, y, z, 0)
+    level.setBlockAt(x+3, y, z, 4)
+    level.setBlockDataAt(x+3, y, z, 0)
+    level.setBlockAt(x+4, y, z, 4)
+    level.setBlockDataAt(x+4, y, z, 0)
+    level.setBlockAt(x+5, y, z, 4)
+    level.setBlockDataAt(x+5, y, z, 0)
+    level.setBlockAt(x, y, z+1, 4)
+    level.setBlockDataAt(x, y, z+1, 0)
+    level.setBlockAt(x+1, y, z+1, 4)
+    level.setBlockDataAt(x+1, y, z+1, 0)
+    level.setBlockAt(x+2, y, z+1, 4)
+    level.setBlockDataAt(x+2, y, z+1, 0)
+    level.setBlockAt(x+3, y, z+1, 4)
+    level.setBlockDataAt(x+3, y, z+1, 0)
+    level.setBlockAt(x+4, y, z+1, 4)
+    level.setBlockDataAt(x+4, y, z+1, 0)
+    level.setBlockAt(x+5, y, z+1, 4)
+    level.setBlockDataAt(x+5, y, z+1, 0)
+    level.setBlockAt(x, y, z+2, 4)
+    level.setBlockDataAt(x, y, z+2, 0)
+    level.setBlockAt(x+1, y, z+2, 4)
+    level.setBlockDataAt(x+1, y, z+2, 0)
+    level.setBlockAt(x+2, y, z+2, 4)
+    level.setBlockDataAt(x+2, y, z+2, 0)
+    level.setBlockAt(x+3, y, z+2, 4)
+    level.setBlockDataAt(x+3, y, z+2, 0)
+    level.setBlockAt(x+4, y, z+2, 4)
+    level.setBlockDataAt(x+4, y, z+2, 0)
+    level.setBlockAt(x+5, y, z+2, 4)
+    level.setBlockDataAt(x+5, y, z+2, 0)
+    level.setBlockAt(x, y, z-1, 4)
+    level.setBlockDataAt(x, y, z-1, 0)
+    level.setBlockAt(x+1, y, z-1, 4)
+    level.setBlockDataAt(x+1, y, z-1, 0)
+    level.setBlockAt(x+2, y, z-1, 4)
+    level.setBlockDataAt(x+2, y, z-1, 0)
+    level.setBlockAt(x+3, y, z-1, 4)
+    level.setBlockDataAt(x+3, y, z-1, 0)
+    level.setBlockAt(x+4, y, z-1, 4)
+    level.setBlockDataAt(x+4, y, z-1, 0)
+    level.setBlockAt(x+5, y, z-1, 4)
+    level.setBlockDataAt(x+5, y, z-1, 0)
+    level.setBlockAt(x, y, z-2, 4)
+    level.setBlockDataAt(x, y, z-2, 0)
+    level.setBlockAt(x+1, y, z-2, 4)
+    level.setBlockDataAt(x+1, y, z-2, 0)
+    level.setBlockAt(x+2, y, z-2, 4)
+    level.setBlockDataAt(x+2, y, z-2, 0)
+    level.setBlockAt(x+3, y, z-2, 4)
+    level.setBlockDataAt(x+3, y, z-2, 0)
+    level.setBlockAt(x+4, y, z-2, 4)
+    level.setBlockDataAt(x+4, y, z-2, 0)
+    level.setBlockAt(x+5, y, z-2, 4)
+    level.setBlockDataAt(x+5, y, z-2, 0)
+    # Finish first row of generation
+    level.setBlockAt(x, y+1, z+1, 5)
+    level.setBlockDataAt(x, y+1, z+1, 0)
+    level.setBlockAt(x, y+1, z-1, 5)
+    level.setBlockDataAt(x, y+1, z-1, 0)
+    # Start Back Wall Generation
+    level.setBlockAt(x+5, y+1, z, 5)
+    level.setBlockDataAt(x+5, y+1, z, 0)
+    level.setBlockAt(x+5, y+1, z+1, 5)
+    level.setBlockDataAt(x+5, y+1, z+1, 0)
+    level.setBlockAt(x+5, y+1, z-1, 5)
+    level.setBlockDataAt(x+5, y+1, z-1, 0)
+    level.setBlockAt(x+5, y+1, z+1, 5)
+    level.setBlockDataAt(x+5, y+1, z+1, 0)
+    # Back Wall Generated
+    level.setBlockAt(x, y+1, z+2, 4)
+    level.setBlockDataAt(x, y+1, z+2, 0)
+    level.setBlockAt(x+1, y+1, z+2, 5)
+    level.setBlockDataAt(x+1, y+1, z+2, 0)
+    level.setBlockAt(x+2, y+1, z+2, 5)
+    level.setBlockDataAt(x+2, y+1, z+2, 0)
+    level.setBlockAt(x+3, y+1, z+2, 5)
+    level.setBlockDataAt(x+3, y+1, z+2, 0)
+    level.setBlockAt(x+4, y+1, z+2, 5)
+    level.setBlockDataAt(x+4, y+1, z+2, 0)
+    level.setBlockAt(x+5, y+1, z+2, 4)
+    level.setBlockDataAt(x+5, y+1, z+2, 0)
+    # Wall One Generated
+    level.setBlockAt(x, y+1, z-2, 4)
+    level.setBlockDataAt(x, y+1, z-2, 0)
+    level.setBlockAt(x+1, y+1, z-2, 5)
+    level.setBlockDataAt(x+1, y+1, z-2, 0)
+    level.setBlockAt(x+2, y+1, z-2, 5)
+    level.setBlockDataAt(x+2, y+1, z-2, 0)
+    level.setBlockAt(x+3, y+1, z-2, 5)
+    level.setBlockDataAt(x+3, y+1, z-2, 0)
+    level.setBlockAt(x+4, y+1, z-2, 5)
+    level.setBlockDataAt(x+4, y+1, z-2, 0)
+    level.setBlockAt(x+5, y+1, z-2, 4)
+    level.setBlockDataAt(x+5, y+1, z-2, 0)
+    # Second Row Generated
+    # Layer #2 Generated
+    level.setBlockAt(x, y+2, z+1, 5)
+    level.setBlockDataAt(x, y+2, z+1, 0)
+    level.setBlockAt(x, y+2, z-1, 5)
+    level.setBlockDataAt(x, y+2, z-1, 0)
+    level.setBlockAt(x, y+2, z+2, 4)
+    level.setBlockDataAt(x, y+2, z+2, 0)
+    level.setBlockAt(x, y+2, z-2, 4)
+    level.setBlockDataAt(x, y+2, z-2, 0)
+    # Front Wall Generated
+    level.setBlockAt(x+1, y+2, z+2, 5)
+    level.setBlockDataAt(x+1, y+2, z+2, 0)
+    level.setBlockAt(x+2, y+2, z+2, 102)
+    level.setBlockDataAt(x+2, y+2, z+2, 0)
+    level.setBlockAt(x+3, y+2, z+2, 5)
+    level.setBlockDataAt(x+3, y+2, z+2, 0)
+    level.setBlockAt(x+4, y+2, z+2, 5)
     
-def end(level, box, options):
-    method = "Generator"
-    print '%s: Ended: %s' % (method, time.ctime())
-    level.markDirtyBox(box)
-
-
 def perform(level, box, options):
-    buil = options["Building"]
     method = "Generator"
     print '%s: Started: %s' % (method, time.ctime())
-    AIRBLOCK = 0
-    bx = box.minx
-    by = box.miny
-    bz = box.minz
+    findin(level, box, options)
+    print '%s: Ended: %s' % (method, time.ctime())
+    level.markDirtyBox(box)
+        
+def findin(level, box, options):
+    buil = options["Building"]
     # Start input finder
-    if buil == "smith":
+    if buil == "1":
         print 'Choose Blacksmith'
 
-    elif buil == "church":
+    elif buil == "2":
         print 'Choose Church'
 
-    elif buil == "lib":
+    elif buil == "3":
         print 'Choose Library'
 
-    elif buil == "h1":
+    elif buil == "4":
         print 'Choose House #1'
-        level.setBlockAt(bx, by, bz, 4)
-        level.setBlockDataAt(bx, by, bz, 0)
-        level.setBlockAt(bx-1, by, bz, 67)
-        level.setBlockDataAt(bx-1, by, bz, 0x0)
-        level.setBlockAt(bx+1, by, bz, 4)
-        level.setBlockDataAt(bx+1, by, bz, 0)
-        level.setBlockAt(bx+2, by, bz, 4)
-        level.setBlockDataAt(bx+2, by, bz, 0)
-        level.setBlockAt(bx+3, by, bz, 4)
-        level.setBlockDataAt(bx+3, by, bz, 0)
-        level.setBlockAt(bx+4, by, bz, 4)
-        level.setBlockDataAt(bx+4, by, bz, 0)
-        level.setBlockAt(bx+5, by, bz, 4)
-        level.setBlockDataAt(bx+5, by, bz, 0)
-        level.setBlockAt(bx, by, bz+1, 4)
-        level.setBlockDataAt(bx, by, bz+1, 0)
-        level.setBlockAt(bx+1, by, bz+1, 4)
-        level.setBlockDataAt(bx+1, by, bz+1, 0)
-        level.setBlockAt(bx+2, by, bz+1, 4)
-        level.setBlockDataAt(bx+2, by, bz+1, 0)
-        level.setBlockAt(bx+3, by, bz+1, 4)
-        level.setBlockDataAt(bx+3, by, bz+1, 0)
-        level.setBlockAt(bx+4, by, bz+1, 4)
-        level.setBlockDataAt(bx+4, by, bz+1, 0)
-        level.setBlockAt(bx+5, by, bz+1, 4)
-        level.setBlockDataAt(bx+5, by, bz+1, 0)
-        level.setBlockAt(bx, by, bz+2, 4)
-        level.setBlockDataAt(bx, by, bz+2, 0)
-        level.setBlockAt(bx+1, by, bz+2, 4)
-        level.setBlockDataAt(bx+1, by, bz+2, 0)
-        level.setBlockAt(bx+2, by, bz+2, 4)
-        level.setBlockDataAt(bx+2, by, bz+2, 0)
-        level.setBlockAt(bx+3, by, bz+2, 4)
-        level.setBlockDataAt(bx+3, by, bz+2, 0)
-        level.setBlockAt(bx+4, by, bz+2, 4)
-        level.setBlockDataAt(bx+4, by, bz+2, 0)
-        level.setBlockAt(bx+5, by, bz+2, 4)
-        level.setBlockDataAt(bx+5, by, bz+2, 0)
-        level.setBlockAt(bx, by, bz-1, 4)
-        level.setBlockDataAt(bx, by, bz-1, 0)
-        level.setBlockAt(bx+1, by, bz-1, 4)
-        level.setBlockDataAt(bx+1, by, bz-1, 0)
-        level.setBlockAt(bx+2, by, bz-1, 4)
-        level.setBlockDataAt(bx+2, by, bz-1, 0)
-        level.setBlockAt(bx+3, by, bz-1, 4)
-        level.setBlockDataAt(bx+3, by, bz-1, 0)
-        level.setBlockAt(bx+4, by, bz-1, 4)
-        level.setBlockDataAt(bx+4, by, bz-1, 0)
-        level.setBlockAt(bx+5, by, bz-1, 4)
-        level.setBlockDataAt(bx+5, by, bz-1, 0)
-        level.setBlockAt(bx, by, bz-2, 4)
-        level.setBlockDataAt(bx, by, bz-2, 0)
-        level.setBlockAt(bx+1, by, bz-2, 4)
-        level.setBlockDataAt(bx+1, by, bz-2, 0)
-        level.setBlockAt(bx+2, by, bz-2, 4)
-        level.setBlockDataAt(bx+2, by, bz-2, 0)
-        level.setBlockAt(bx+3, by, bz-2, 4)
-        level.setBlockDataAt(bx+3, by, bz-2, 0)
-        level.setBlockAt(bx+4, by, bz-2, 4)
-        level.setBlockDataAt(bx+4, by, bz-2, 0)
-        level.setBlockAt(bx+5, by, bz-2, 4)
-        level.setBlockDataAt(bx+5, by, bz-2, 0)
-        # Finish first row of generation
-        level.setBlockAt(bx, by+1, bz+1, 5)
-        level.setBlockDataAt(bx, by+1, bz+1, 0)
-        level.setBlockAt(bx, by+1, bz-1, 5)
-        level.setBlockDataAt(bx, by+1, bz-1, 0)
-        # Start Back Wall Generation
-        level.setBlockAt(bx+5, by+1, bz, 5)
-        level.setBlockDataAt(bx+5, by+1, bz, 0)
-        level.setBlockAt(bx+5, by+1, bz+1, 5)
-        level.setBlockDataAt(bx+5, by+1, bz+1, 0)
-        level.setBlockAt(bx+5, by+1, bz-1, 5)
-        level.setBlockDataAt(bx+5, by+1, bz-1, 0)
-        level.setBlockAt(bx+5, by+1, bz+1, 5)
-        level.setBlockDataAt(bx+5, by+1, bz+1, 0)
-        # Back Wall Generated
-        level.setBlockAt(bx, by+1, bz+2, 4)
-        level.setBlockDataAt(bx, by+1, bz+2, 0)
-        level.setBlockAt(bx+1, by+1, bz+2, 5)
-        level.setBlockDataAt(bx+1, by+1, bz+2, 0)
-        level.setBlockAt(bx+2, by+1, bz+2, 5)
-        level.setBlockDataAt(bx+2, by+1, bz+2, 0)
-        level.setBlockAt(bx+3, by+1, bz+2, 5)
-        level.setBlockDataAt(bx+3, by+1, bz+2, 0)
-        level.setBlockAt(bx+4, by+1, bz+2, 5)
-        level.setBlockDataAt(bx+4, by+1, bz+2, 0)
-        level.setBlockAt(bx+5, by+1, bz+2, 4)
-        level.setBlockDataAt(bx+5, by+1, bz+2, 0)
-        # Wall One Generated
-        level.setBlockAt(bx, by+1, bz-2, 4)
-        level.setBlockDataAt(bx, by+1, bz-2, 0)
-        level.setBlockAt(bx+1, by+1, bz-2, 5)
-        level.setBlockDataAt(bx+1, by+1, bz-2, 0)
-        level.setBlockAt(bx+2, by+1, bz-2, 5)
-        level.setBlockDataAt(bx+2, by+1, bz-2, 0)
-        level.setBlockAt(bx+3, by+1, bz-2, 5)
-        level.setBlockDataAt(bx+3, by+1, bz-2, 0)
-        level.setBlockAt(bx+4, by+1, bz-2, 5)
-        level.setBlockDataAt(bx+4, by+1, bz-2, 0)
-        level.setBlockAt(bx+5, by+1, bz-2, 4)
-        level.setBlockDataAt(bx+5, by+1, bz-2, 0)
-        # Second Row Generated
-        
-        
-        
+        house1(level, box.minx, box.miny, box.minz)
         
 
-    elif buil == "h2":
+    elif buil == "5":
         print 'Choose House #2'
 
-    elif buil == "h3":
+    elif buil == "6":
         print 'Choose House #3'
 
-    elif buil == "foun":
+    elif buil == "7":
         print 'Choose Fountain'
         
 
-    elif buil == "farm":
+    elif buil == "8":
         print 'Choose Farm'
-        
+    
