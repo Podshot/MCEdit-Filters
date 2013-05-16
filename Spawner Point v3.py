e7542fcc32b96b26d035b423257730a7377323ad
Podshot <ben.gothard3@gmail.com>
2013-05-15 22:19:32 -0300

    Update Spawner Point v3.py
    Accidently used older, non-functioning version. Refactored filter and localized everything to the perform function.

--- Spawner Point v3.py
+++ Spawner Point v3.py
@@ -1,4 +1,3 @@
-from pymclevel import MCSchematic
 from pymclevel import TileEntity
 from pymclevel import TAG_List
 from pymclevel import TAG_Byte
@@ -12,42 +11,72 @@ from math import sqrt, tan, sin, cos, pi, ceil, floor, acos, atan, asin, degrees
 from random import *
 from numpy import *
 from pymclevel import alphaMaterials
+from pymclevel import MCSchematic
 import time
 from pymclevel import TileEntity
 
 displayName = "Spawner Point v3"
 
 inputs = (
-    ("Keep Radius?", False),
+    ("Change Spawn Radius?", False),
+    ("Replaces the Spawning Coordinates to a Sponge block in the Selection box.", "label"),
 )
 
-def Findblock(level, box):
-    targets = []
+
+def perform(level, box, options):
+    method = "Spawner Pointer"
+    xend = []
+    yend = []
+    zend = []
+    radi = options["Change Spawn Radius?"]
+    print '%s: Started at: %s' % (method, time.ctime())
+
     for x in xrange(box.minx, box.maxx):
         for y in xrange(box.miny, box.maxy):
             for z in xrange(box.minz, box.maxz):
                 block = level.blockAt(x, y, z)
-                if block == 35:
-                    data = level.blockDataAt(x, y, z)
-                    if data == 4:
-                        targets.append((x, y,z))
-
-def perfom(level, box, options):
-    radi = options["Keep Radius?"]
-
-    targets = FindBlock(level, box)
+                if block == 19:
+                    xend.append(x)
+                    yend.append(y)
+                    zend.append(z)
 
     for (chunk, slices, point) in level.getChunkSlices(box):
+        for e in chunk.Entities:
+            if e["id"].value == "MinecartSpawner":
+                x2 = e["x"].value
+                y2 = e["y"].value
+                z2 = e["z"].value
+
+                if x2 >= box.minx and x2 < box.maxx and y2 >=box.miny and y2 < box.maxy and z2 >= box.minz and z2 < box.maxz:
+                    if "SpawnData" in e:
+                        pos = e["Spawndata"]
+                        pos["Pos"] = TAG_List()
+                        pos["Pos"].append(TAG_Double(xend+0.5))
+                        pos["Pos"].append(TAG_Double(yend+0.5))
+                        pos["Pos"].append(TAG_Double(zend+0.5))
+                        del e["SpawnPotentials"]
+                        if radi:
+                            e["SpawnRange"] = TAG_Short(1)
+                        print '%s %s %s' % (xend, yend, zend)
+                        print '%s: Ended at: %s' % (method, time.ctime())
+                        level.markDirtyBox(box)
+                    
+                    
         for t in chunk.TileEntities:
             x1 = t["x"].value
             y1 = t["y"].value
             z1 = t["z"].value
 
             if x1 >= box.minx and x1 < box.maxx and y1 >= box.miny and y1 < box.maxy and z1 >= box.minz and z1 < box.maxz and t["id"].value == "MobSpawner":
-
-               pos = e["Spawndata"]
-               pos["Pos"] = TAG_List()
-               print '%s %s %s' % (targets)
-               chunk.dirty = True
-
-    
+                if "SpawnData" in t:
+                    pos = t["Spawndata"]
+                    pos["Pos"] = TAG_List()
+                    pos["Pos"].append(TAG_Double(xend+0.5))
+                    pos["Pos"].append(TAG_Double(yend+0.5))
+                    pos["Pos"].append(TAG_Double(zend+0.5))
+                    del t["Spawnpotentials"]
+                    if radi:
+                        t["SpawnRange"] = TAG_Short(1)
+                    print '%s %s %s' % (xend, yend, zend)
+                    print '%s: Ended at: %s' % (method, time.ctime())
+                    level.markDirtyBox(box) 
