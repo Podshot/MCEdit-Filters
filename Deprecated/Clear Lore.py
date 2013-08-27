9e35aeb38430ac5d68450265971631795f833db4
Podshot <ben.gothard3@gmail.com>
2013-08-27 12:58:28 -0400

    Moved Clear Lore to Deprecated

---
+++ Deprecated/Clear Lore.py
@@ -0,0 +1,22 @@
+from pymclevel.nbt import TAG_Byte, TAG_Short, TAG_String, TAG_Compound, TAG_Int, TAG_List
+
+displayName = "Clear Lore"
+		
+def perform(level, box, options):
+	item = options["Item"]
+	
+	for (chunk, slices, point) in level.getChunkSlices(box):
+		for te in chunk.TileEntities:
+			if te["id"].value == "Chest":
+				x = te["x"].value
+                                y = te["y"].value
+                                z = te["z"].value
+				
+				if (x,y,z) in box:
+					for item in te["Items"]:
+                                                if ["tag"] in item:
+                                                        if ["display"] in item["tag"]:
+                                                                if ["Lore"] in item["tag"]["display"]:
+                                                                        del item["tag"]["display"]["Lore"]
+							
+									chunk.dirty = True
