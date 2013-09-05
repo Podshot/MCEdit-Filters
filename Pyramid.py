6688133cac7893dff484daa99985f6ed9157f00b
Podshot <ben.gothard3@gmail.com>
2013-09-05 08:15:24 -0400

    Updated Pyramind Filter

--- Pyramid.py
+++ Pyramid.py
@@ -6,7 +6,8 @@ import time
 displayName = "Pyramid Maker"
 
 inputs = (
-    ("Levels", (1,1,10)),
+    ("Levels", (2,2,10)),
+    ("Box must be 1x1x1!","label"),
     )
 
 def lvl1():
@@ -16,8 +17,16 @@ def lvl1():
     return e
 
 def lvl2():
+    e = MCSchematic(shape=(5,1,5),filename='')
+    e._Blocks = [[[19,19,19,19,19],[19,19,19,19,19],[19,19,19,19,19],[19,19,19,19,19],[19,19,19,19,19]]]
+    e.root_tag['Data'] = TAG_Byte_Array([[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]])
+    return e
 
 def lvl3():
+    e = MCSchematic(shape=(7,1,7),filename='')
+    e._Blocks = [[[19,19,19,19,19,19,19],[19,19,19,19,19,19,19],[19,19,19,19,19,19,19],[19,19,19,19,19,19,19],[19,19,19,19,19,19,19],[19,19,19,19,19,19,19],[19,19,19,19,19,19,19]]]
+    e.root_tag['Data'] = TAG_Byte_Array([[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]])
+    return e
 
 def lvl4():
 
@@ -48,10 +57,17 @@ def levelOne(level, dest):
     vec = Vector(0,0,0)
     level.copyBlocksFrom(l1,l1.bounds,vec + dest)
 
-def levelTwo(level, dest):
+def levelTwo(level, dest, box):
     vec = Vector(0,0,0)
     level.copyBlocksFrom(l2,l2.bounds,vec + dest)
+    level.markDirtyBox(box)
     levelOne(level, dest)
+
+def levelThree(level, dest, box):
+    vec = Vector(0,0,0)
+    level.copyBlocksFrom(l3,l3.bounds,vec + dest)
+    level.markDirtyBox(box)
+    levelTwo(level, dest, box)
 
 
 
@@ -64,6 +80,28 @@ def levelTwo(level, dest):
 
 
 def perform(level, box, options):
+    levels = options["Levels"]
+    boxx = box.maxx - box.minx
+    boxy = box.maxy - box.miny
+    boxz = box.maxz - box.minz
+    if boxx != 1:
+        raise Exception("Box must be 1x1x1!")
+    if boxy != 1:
+        raise Exception("Box must be 1x1x1!")
+    if boxz != 1:
+        raise Exception("Box must be 1x1x1!")
     level.setBlockAt(box.minx, box.miny, box.minz, 19)
     level.setBlockDataAt(box.minx, box.miny, box.minz, 0)
+    if levels == 2:
+        levelOne(level,[box.minx,box.miny,box.minz])
+        level.markDirtyBox(box)
+    if levels == 3:
+        levelTwo(level,[box.minx,box.miny,box.minz], box)
+        level.markDirtyBox(box)
+    if levels == 4:
+        levelThree(level,[box.minx,box.miny,box.minz], box)
+        level.markDirtyBox(box)
+    if levels == 5:
+        levelFour(level,[box.minx,box.miny,box.minz], box)
+        level.markDirtyBox(box)
     
