3118e304493f384e3570bd61ec55154b68ff1ed9
Podshot <ben.gothard3@gmail.com>
2013-09-05 11:18:48 -0400

    Contiued work on Pyramid Levels 6 - 10 will be used, I just pu the print statement so I can test the filter

--- Pyramid.py
+++ Pyramid.py
@@ -29,18 +29,31 @@ def lvl3():
     return e
 
 def lvl4():
+    e = MCSchematic(shape=(7,1,7),filename='')
+    e._Blocks = [[[19,19,19,19,19,19,19],[19,19,19,19,19,19,19],[19,19,19,19,19,19,19],[19,19,19,19,19,19,19],[19,19,19,19,19,19,19],[19,19,19,19,19,19,19],[19,19,19,19,19,19,19]]]
+    e.root_tag['Data'] = TAG_Byte_Array([[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]])
+    return e
 
 def lvl5():
+    e = MCSchematic(shape=(9,1,9),filename='')
+    e._Blocks = [[[19,19,19,19,19,19,19,19,19],[19,19,19,19,19,19,19,19,19],[19,19,19,19,19,19,19,19,19],[19,19,19,19,19,19,19,19,19],[19,19,19,19,19,19,19,19,19],[19,19,19,19,19,19,19,19,19],[19,19,19,19,19,19,19,19,19],[19,19,19,19,19,19,19,19,19],[19,19,19,19,19,19,19,19,19]]]
+    e.root_tag['Data'] = TAG_Byte_Array([[[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]])
+    return e
 
 def lvl6():
+    print "Not Used"
 
 def lvl7():
+    print "Not Used"
 
 def lvl8():
+    print "Not Used"
 
 def lvl9():
+    print "Not Used"
 
 def lvl10():
+    print "Not Used"
 
 l1 = lvl1()
 l2 = lvl2()
@@ -54,20 +67,32 @@ l9 = lvl9()
 l10 = lvl10()
 
 def levelOne(level, dest):
-    vec = Vector(0,0,0)
+    vec = Vector(0,-1,0)
     level.copyBlocksFrom(l1,l1.bounds,vec + dest)
 
 def levelTwo(level, dest, box):
-    vec = Vector(0,0,0)
+    vec = Vector(0,-2,0)
     level.copyBlocksFrom(l2,l2.bounds,vec + dest)
     level.markDirtyBox(box)
     levelOne(level, dest)
 
 def levelThree(level, dest, box):
-    vec = Vector(0,0,0)
+    vec = Vector(0,-3,0)
     level.copyBlocksFrom(l3,l3.bounds,vec + dest)
     level.markDirtyBox(box)
     levelTwo(level, dest, box)
+
+def levelFour(level, dest, box):
+    vec = Vector(0,-4,0)
+    level.copyBlocksFrom(l4,l4.bounds,vec + dest)
+    level.markDirtyBox(box)
+    levelThree(level, dest, box)
+
+def levelFive(level, dest, box):
+    vec = Vector(0,-5,0)
+    level.coptBlocksFrom(l5,l5.bounds,vec + dest)
+    level.markDirtyBox(box)
+    levelFour(level, dest, box)
 
 
 
@@ -103,5 +128,8 @@ def perform(level, box, options):
         level.markDirtyBox(box)
     if levels == 5:
         levelFour(level,[box.minx,box.miny,box.minz], box)
+        level.markDirtyBox(box)
+    if levels == 6:
+        levelFive(level,[box.minx,box.miny,box.minz], box)
         level.markDirtyBox(box)
     
