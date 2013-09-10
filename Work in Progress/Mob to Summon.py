3fe247dd8b71a54fc916c0a21a437d8216a5dc93
Podshot <ben.gothard3@gmail.com>
2013-09-10 11:24:28 -0400

    Fixed bugs in the filter

--- Work in Progress/Mob to Summon.py
+++ Work in Progress/Mob to Summon.py
@@ -4,7 +4,7 @@ from pymclevel import TileEntity
 displayName = "Mob to Summon"
 
 inputs = (
-    ("Does not support extra tags yet.!/n(Equipment, potion effects)", "label"),
+    ("Does not support extra tags yet!(Equipment, potion effects)", "label"),
     )
 
 def perform(level, box, options):
@@ -15,6 +15,9 @@ def perform(level, box, options):
             x = e["Pos"][0].value
             y = e["Pos"][1].value
             z = e["Pos"][2].value
+            print "x: %s" % (x)
+            print "y: %s" % (y)
+            print "z: %s" % (z)
 
             if (x,y,z) in box:
                 entitiesToRemove.append((chunk, e))
@@ -23,14 +26,24 @@ def perform(level, box, options):
                 # End gathering mob info
                 com = "/summon "+ mid +" ~ ~ ~"
                 print com
-                level.setBlockAt(x, y+0.5, z, 137)
+                level.setBlockAt(box.maxx, box.maxy, box.maxz, 137)
+                level.setBlockDataAt(box.maxx, box.maxy, box.maxz, 0)
 
                 control = TAG_Compound()
-                control["id"] = TAG_String("Control"]
+                control["id"] = TAG_String("Control")
                 control["Command"] = TAG_String(com)
-                control["x"] = TAG_Int(x)
-                control["y"] = TAG_Int(y+0.5)
-                control["z"] = TAG_Int(z)
+                control["x"] = TAG_Int(box.maxx)
+                control["y"] = TAG_Int(box.maxy)
+                control["z"] = TAG_Int(box.maxz)
+                control["CustomName"] = TAG_String("@")
+                control["TrackOutput"] = TAG_Byte(1)
+                control["SuccesCount"] = TAG_Int(0)
+                chunk.TileEntities.append(control)
+                chunk.dirty = True
+                
+    for (chunk, e) in entitiesToRemove:
+        chunk.Entities.remove(e)
+                
                 
                 
                 
