7d911e7618fcfe07fb15e01a2b9b96ef10e9978b
Podshot <ben.gothard3@gmail.com>
2013-09-17 18:06:42 -0400

    Decreased wait time and added if statement to stop creation of air setblock commands

--- block2command.py
+++ block2command.py
@@ -19,20 +19,21 @@ def perform(level, box, options):
 	 	 	for z in xrange(box.minz, box.maxz):
 	 	 	 	bid = int(level.blockAt(x, y, z))
 	 	 	 	bda = int(level.blockDataAt(x, y, z))
-	 	 	 	time.sleep(0.5)
-	 	 	 	level.setBlockAt(x, y, z, 137)
-	 	 	 	com = "/setblock " + str(x) + " " + str(y) + " " + str(z - 1) + " " + str(bid) + " " + str(bda) + " " + str(met)
-	 	 	 	print com
-	 	 	 	command = TAG_Compound()
-	 	 	 	command["id"] = TAG_String("Control")
-	 	 	 	command["Command"] = TAG_String(com)
-	 	 	 	command["x"] = TAG_Int(x)
-	 	 	 	command["y"] = TAG_Int(y)
-	 	 	 	command["z"] = TAG_Int(z)
-	 	 	 	command["CustomName"] = TAG_String("@")
-	 	 	 	command["TrackOutput"] = TAG_Byte(1)
-	 	 	 	command["SuccesCount"] = TAG_Int(0)
-	 	 	 	chunk = level.getChunk(box.minx/16, box.minz/16)
-                                chunk.TileEntities.append(command)
-	 	 	 	chunk.dirty = True
+	 	 	 	if bid != 0:
+                                        time.sleep(0.25)
+                                        level.setBlockAt(x, y, z, 137)
+                                        com = "/setblock " + str(x) + " " + str(y) + " " + str(z - 1) + " " + str(bid) + " " + str(bda) + " " + str(met)
+                                        print com
+                                        command = TAG_Compound()
+                                        command["id"] = TAG_String("Control")
+                                        command["Command"] = TAG_String(com)
+                                        command["x"] = TAG_Int(x)
+                                        command["y"] = TAG_Int(y)
+                                        command["z"] = TAG_Int(z)
+                                        command["CustomName"] = TAG_String("@")
+                                        command["TrackOutput"] = TAG_Byte(1)
+                                        command["SuccesCount"] = TAG_Int(0)
+                                        chunk = level.getChunk(box.minx/16, box.minz/16)
+                                        chunk.TileEntities.append(command)
+                                        chunk.dirty = True
 	 	 	 	
