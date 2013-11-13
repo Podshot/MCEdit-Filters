34f0ef3041e10e0a30e35ab3803bb60f628b5c46
Podshot <ben.gothard3@gmail.com>
2013-11-13 10:04:02 -0500

    Redesigned option gathering. Thanks to tomsik68

--- Command Replace.py
+++ Command Replace.py
@@ -13,29 +13,31 @@ inputs = (
     ("Filter Word #4:",("string","value=")),
     ("Replace Word #4:",("string","value=")),
     ("Filter Word #5:",("string","value=")),
-    ("Replace Word #6:",("string","value=")),
+    ("Replace Word #5:",("string","value=")),
     )
 
-def replacer(string, word1, word2, word3, word4, word5, filter1, filter2, filter3, filter4, filer5):
-    string = string.replace(word1, filter1)
-    string = string.replace(word2, filter2)
-    string = string.replace(word3, filter3)
-    string = string.replace(word4, filter4)
-    string = string.replace(word5, filter5)
+def replacer(string, replaceWords):
+    for (word,replacement) in replaceWords.items():
+        string = string.replace(words,replacement)
     return string
 
 def perform(level, box, options):
+    replaceWords = {}
+
+    for i in range(1,5):
+        replaceWords[options["Filter Word #"+str(i)]] = options["Replace Word #"+str(i)]
+        
     # Option Gathering
-    FW1 = options["Filter Word #1:"]
-    FW2 = options["Filter Word #2:"]
-    FW3 = options["Filter Word #3:"]
-    FW4 = options["Filter Word #4:"]
-    FW5 = options["Filter Word #5:"]
-    RW1 = options["Replace Word #1:"]
-    RW2 = options["Replace Word #2:"]
-    RW3 = options["Replace Word #3:"]
-    RW4 = options["Replace Word #4:"]
-    RW5 = options["Replace Word #5:"]
+    #FW1 = options["Filter Word #1:"]
+    #FW2 = options["Filter Word #2:"]
+    #FW3 = options["Filter Word #3:"]
+    #FW4 = options["Filter Word #4:"]
+    #FW5 = options["Filter Word #5:"]
+    #RW1 = options["Replace Word #1:"]
+    #RW2 = options["Replace Word #2:"]
+    #RW3 = options["Replace Word #3:"]
+    #RW4 = options["Replace Word #4:"]
+    #RW5 = options["Replace Word #5:"]
     # End
 
     for (chunk, slices, point) in level.getChunkSlices(box):
@@ -48,5 +50,5 @@ def perform(level, box, options):
                 if (x,y,z) in box:
                     command = te["Command"].value
                     # Replace stuff
-                    t["Command"] = TAG_String(replacer(Line1, FW1, FW2, FW3, FW4, FW5, RW1, RW2, RW3, RW4, RW4)))
+                    te["Command"] = TAG_String(replacer(command,replaceWords))
                     chunk.dirty = True
