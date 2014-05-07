d4f55d97d4d27f42395a8be13530cca9b89a7db9
Podshot <ben.gothard3@gmail.com>
2014-05-07 11:32:53 -0400

    Updated filters

--- Head to UUID.py
+++ Head to UUID.py
@@ -3,10 +3,6 @@ import urllib2, json, time
 
 URL = "http://api.goender.net/api/profiles/"
 
-inputs = (
-     ("Player Name:", ("string", "value=")),
-     )
-
 def perform(level, box, options):
      
      for (chunk, point, slices) in level.getChunkSlices(box):
@@ -17,24 +13,13 @@ def perform(level, box, options):
                  z = te["z"].value
 
                  if (x,y,z) in box:
-                     if "ExtraType" not in te:
-                         # Assuming user has already converted world to a 1.8 Snapshot
-                         player = str(options["Player Name:"])
-                         site = urllib2.urlopen(URL + player)
-                         response = site.read()
-                         jsonRaw = json.loads(response)
-                         te["Owner"] = TAG_Compound()
-                         te["Owner"]["Id"] = TAG_String(jsonRaw[player])
-                         te["Owner"]["Name"] = TAG_String(player)
-                         chunk.dirty = True
-                    else:
-                         name = te["ExtraType"].value
-                         site = urllib2.urlopen(URL + name)
-                         response = site.read()
-                         jsonRaw = json.loads(response)
-                         te["Owner"] = TAG_Compound()
-                         te["Owner"]["Id"] = TAG_String(jsonRaw[player])
-                         te["Owner"]["Name"] = TAG_String(player)
-                         chunk.dirty = True
+                    name = te["ExtraType"].value
+                    site = urllib2.urlopen(URL + name)
+                    response = site.read()
+                    jsonRaw = json.loads(response)
+                    te["Owner"] = TAG_Compound()
+                    te["Owner"]["Id"] = TAG_String(jsonRaw[player])
+                    te["Owner"]["Name"] = TAG_String(player)
+                    chunk.dirty = True
                          
                          
--- ImgurToHeadTexture.py
+++ ImgurToHeadTexture.py
@@ -2,7 +2,7 @@ import base64, random
 from pymclevel.nbt import *
 from mcplatform import askOpenFile
 
-UUIDS = ["89e8ad73-b2e5-4502-8b74-dab92921c8c1", "9196230b-deac-46f2-8b86-ffb7cbc566c0", "d1d6fe65-6df9-422f-9e69-da2c694cc764", "9a891f3d-6573-45da-b42a-52293c37ff7c", "14878d90-01f3-4845-afda-97cce8df5771", "f352f7d9-f1f4-493f-9f87-699c28229191", "e031eda9-f6fb-43cf-a291-8d33fbed0ef6", "ff455523-909b-4bfe-85d6-62ba4ab72796", "8ab1deac-5e61-4911-aca1-3256e00d57dc", "6ec1592a-b14b-4446-a8e2-7678ff56b5ad", "d8c7efab-ffc4-42c3-b537-aae129e72863"]
+UUIDS = ["89e8ad73-b2e5-4502-8b74-dab92921c8c1", "9196230b-deac-46f2-8b86-ffb7cbc566c0", "d1d6fe65-6df9-422f-9e69-da2c694cc764", "9a891f3d-6573-45da-b42a-52293c37ff7c", "14878d90-01f3-4845-afda-97cce8df5771", "f352f7d9-f1f4-493f-9f87-699c28229191", "e031eda9-f6fb-43cf-a291-8d33fbed0ef6", "ff455523-909b-4bfe-85d6-62ba4ab72796", "8ab1deac-5e61-4911-aca1-3256e00d57dc", "6ec1592a-b14b-4446-a8e2-7678ff56b5ad", "d8c7efab-ffc4-42c3-b537-aae129e72863", "9620f7db-cee2-4b46-97d9-bc8b5ac81333", "fc290946-5c45-41a0-85c1-88a7c3283758", "41033658-d057-4e96-bbdc-b30475fac7da", "e1b2c6d0-d9fc-4ea3-9e95-8f09ded48ec9"]
 CHUNKSIZE = 16
 USEFILE = False
 
