db460f5b972eee6748ed22c5ca020be17659a6bb
Podshot <ben.gothard3@gmail.com>
2014-05-08 14:13:50 -0400

    Added Try statement and changed an options

--- Update Filters.py
+++ Update Filters.py
@@ -5,27 +5,32 @@ METHOD = "[Update Filters]"
 
 inputs = (
     ("Remove Old Filters?", True),
-    ("View Changelogs", False),
+    ("View Change Logs", False),
     )
 
 def perform(level, box, options):
     doRemove = options["Remove Old Filters?"]
-    changeLog = options["View Changelogs"]
-    filters = glob.glob("filters/*.py")
+    changeLog = options["View Change Logs"]
+    filters = glob.glob("*.py")
     for filt in filters:
-        py = __import__(filt)
-        filterUpdateURL = str(py.UPDATE_URL)
-        filterVersion = str(py.VERSION)
-        site = urllib2.urlopen(filterUpdateURL)
-        response = site.read()
-        jsonRaw = json.loads(response)
-        if filterVersion != str(jsonRaw["Version"]):
-            urllib.urlretrieve(str(jsonRaw["Download-URL"]), str(jsonRaw["Name"]))
-            print '%s: Updated \"%s\" from version %s to version %s' % (METHOD, py.displayName, py.VERSION, str(jsonRaw["Version"]))
-            if doRemove:
-                os.remove(filt)
-            if changeLog:
-                log = str(jsonRaw["ChangeLog"])
-                webbrowser.open_new_tab(log)
-        else:
-            print '%s: %s\'s version matched update the site\'s version' % (METHOD, py.displayName)
+        try:
+            fileName = filt.split(".")
+            name = fileName[0]
+            py = __import__(name)
+            filterUpdateURL = str(py.UPDATE_URL)
+            filterVersion = str(py.VERSION)
+            site = urllib2.urlopen(filterUpdateURL)
+            response = site.read()
+            jsonRaw = json.loads(response)
+            if filterVersion != str(jsonRaw["Version"]):
+                urllib.urlretrieve(str(jsonRaw["Download-URL"]), str(jsonRaw["Name"]))
+                print '%s: Updated "%s" from version %s to version %s' % (METHOD, py.displayName, py.VERSION, str(jsonRaw["Version"]))
+                if doRemove:
+                    os.remove(filt)
+                if changeLog:
+                    log = str(jsonRaw["ChangeLog"])
+                    webbrowser.open_new_tab(log)
+            else:
+                print '%s: %s\'s version matched update the site\'s version' % (METHOD, py.displayName)
+        except:
+                pass
