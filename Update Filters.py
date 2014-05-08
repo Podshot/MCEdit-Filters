5b302c74afe79aef08a836c82e8e42673703b5ea
Podshot <ben.gothard3@gmail.com>
2014-05-08 11:36:11 -0400

    Can now view a changelog and logs updating process

--- Update Filters.py
+++ Update Filters.py
@@ -1,13 +1,16 @@
-import glob, urllib2, urllib, json, os
+import glob, urllib2, urllib, json, os, webbrowser
 
 displayName = "Update Filters"
+METHOD = "[Update Filters]"
 
 inputs = (
     ("Remove Old Filters?", True),
+    ("View Changelogs", False),
     )
 
 def perform(level, box, options):
     doRemove = options["Remove Old Filters?"]
+    changeLog = options["View Changelogs"]
     filters = glob.glob("filters/*.py")
     for filt in filters:
         py = __import__(filt)
@@ -18,6 +21,11 @@ def perform(level, box, options):
         jsonRaw = json.loads(response)
         if filterVersion != str(jsonRaw["Version"]):
             urllib.urlretrieve(str(jsonRaw["Download-URL"]), str(jsonRaw["Name"]))
+            print '%s: Updated \"%s\" from version %s to version %s' % (METHOD, py.displayName, py.VERSION, str(jsonRaw["Version"]))
             if doRemove:
                 os.remove(filt)
-            
+            if changeLog:
+                log = str(jsonRaw["ChangeLog"])
+                webbrowser.open_new_tab(log)
+        else:
+            print '%s: %s\'s version matched update the site\'s version' % (METHOD, py.displayName)
